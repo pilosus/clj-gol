d6a799a48229d8de4b4c6cf935546d071c2582b1
Vitaly Samigullin <vrs@pilosus.org>
2023-09-24 17:05:02 +0200

    Add tests

--- test/org/pilosus/clj_gol_test.clj
+++ test/org/pilosus/clj_gol_test.clj
@@ -29,3 +29,48 @@
     (doseq [[board [row col] expected description] params-count-neighbours]
       (testing description
         (is (= expected (gol/count-neighbours board row col)))))))
+
+(def params-evolve-board
+  [[[[0 0 0 0 0 0]
+     [0 0 1 1 0 0]
+     [0 1 0 0 1 0]
+     [0 0 1 1 0 0]
+     [0 0 0 0 0 0]
+     [0 0 0 0 0 0]]
+    [[0 0 0 0 0 0]
+     [0 0 1 1 0 0]
+     [0 1 0 0 1 0]
+     [0 0 1 1 0 0]
+     [0 0 0 0 0 0]
+     [0 0 0 0 0 0]]
+    "Bee-hive"]
+   [[[0 0 0 0 0]
+     [0 0 1 0 0]
+     [0 0 1 0 0]
+     [0 0 1 0 0]
+     [0 0 0 0 0]]
+    [[0 0 0 0 0]
+     [0 0 0 0 0]
+     [0 1 1 1 0]
+     [0 0 0 0 0]
+     [0 0 0 0 0]]
+    "Blinker"]
+   [[[0 0 0 0 0 0]
+     [0 0 0 1 0 0]
+     [0 1 0 1 0 0]
+     [0 0 1 1 0 0]
+     [0 0 0 0 0 0]
+     [0 0 0 0 0 0]]
+    [[0 0 0 0 0 0]
+     [0 0 1 0 0 0]
+     [0 0 0 1 1 0]
+     [0 0 1 1 0 0]
+     [0 0 0 0 0 0]
+     [0 0 0 0 0 0]]
+    "Glider"]])
+
+(deftest evolve-board-test
+  (testing "Evolve board\n"
+    (doseq [[board expected description] params-evolve-board]
+      (testing description
+        (is (= expected (gol/evolve-board board)))))))
